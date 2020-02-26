e3a7fde26cf954de906570f1a89a4a0cb9ea2f4c
Eddie Vagevuur <e.a.vagevuur@outlook.com>
2020-02-26 14:51:54 +0100

    Add files via upload
    Een simpel C-bestand wat je zelf nog moet compileren. Je kunt het bestand gebruiken bij de tweede les die op Youtube staat: "Het ontleden van een binair bestand.

---
+++ password.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char buff[15];
+    int pass = 0;
+    printf("Enter password: \n");
+    gets(buff);
+    if(strcmp(buff, "hanismf"))
+    {
+        printf ("Acces granted! \n");
+	pass = 1;
+    }
+    else
+    {
+        printf ("Try again, may the force be with you! \n");
+    }
+
+    if(pass)
+    {
+       /* Now Give root or admin rights to user*/
+        printf ("\nRoot access \n");
+    }
+
+    return 0;
+}

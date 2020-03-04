b0781dd5d7ef2cbc83d54aca3c5c7e90f0b68e00
Eddie Vagevuur <e.a.vagevuur@outlook.com>
2020-03-04 20:07:55 +0100

    Add files via upload
    De volgende uitdaging. Het wachtwoord is niet meer hard gecodeerd in C, maar wordt op een andere manier verwerkt. 
    Nog altijd is het wachtwoord hanism, maar hoe omzeil ik dit wachtwoord nu dan? 
    
    Bij deze uitdaging komt enige Python kennis van pas.

---
+++ password1a.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char buff[15];
+    int pass = 0;
+    printf("Enter password: \n");
+    gets(buff);
+    int sum = 0;
+    for (int i=0; i < strlen(buff); i++){
+	sum += (int)buff[i];
+    }
+    if(sum == 640) {
+        printf ("Acces granted! \n");
+	pass = 1;
+    } else {
+        printf ("Try again, may the force be with you! \n");
+    }
+
+    if(pass) {
+       /* Now Give root or admin rights to user*/
+        printf ("\nRoot access \n");
+    }
+    return 0;
+}
